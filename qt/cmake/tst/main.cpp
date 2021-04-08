f9ee606b6879a12613273d39b095029630646bcf
AlexVlv <alexey.v.v@mail.ru>
2021-04-08 18:20:32 +0300

    QT cmake testings: minimal generated project

---
+++ qt/cmake/tst/main.cpp
@@ -0,0 +1,10 @@
+#include <QCoreApplication>
+#include <QDebug>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    qDebug() << "Hello, world!";
+    exit(0);
+    return a.exec();
+}
