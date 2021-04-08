b97cb437a7e4af672c7af9ae2498051e4a5c52f5
AlexVlv <alexey.v.v@mail.ru>
2021-04-08 18:52:59 +0300

    globbing sources and verbose build

--- qt/cmake/tst/main.cpp
+++ qt/cmake/tst/main.cpp
@@ -1,10 +1,19 @@
+/*
+$Id$
+*/
+
+#include "macro.h"
+
 #include <QCoreApplication>
 #include <QDebug>
+
+
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     qDebug() << "Hello, world!";
+    qDebug() << xstr(SIGN(5));
     exit(0);
     return a.exec();
 }
