2f4be7d0860d6391c97995b87811ca28de51b31d
AlexVlv <alexey.v.v@mail.ru>
2021-04-12 17:32:23 +0300

    cmake distclean target

---
+++ qt/cmake/tst/scripts/distclean.cmake
@@ -0,0 +1,16 @@
+# $Id$
+
+set(cmake_generated
+    ${CMAKE_BINARY_DIR}/CMakeCache.txt
+    ${CMAKE_BINARY_DIR}/cmake_install.cmake
+    ${CMAKE_BINARY_DIR}/Makefile
+    ${CMAKE_BINARY_DIR}/CMakeFiles
+    ${CMAKE_BINARY_DIR}/.cmake
+    ${CMAKE_BINARY_DIR}/install_manifest.txt
+)
+
+foreach(file ${cmake_generated})
+  if (EXISTS ${file})
+     file(REMOVE_RECURSE ${file})
+  endif()
+endforeach(file)
