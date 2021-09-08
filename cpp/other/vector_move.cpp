73629cbe5ad4eb6ec9368d97abb097cb0028596a
Alex <alexey.v.v@mail.ru>
2021-09-08 23:25:08 +0300

    Return vector from function

--- cpp/other/vector_move.cpp
+++ cpp/other/vector_move.cpp
@@ -23,12 +23,20 @@ static void print_vector_info(const std::vector<int> &vector)
 		std::cerr << std::endl;
 }
 
+static std::vector<int> return_vector()
+{
+	std::cerr <<  "local vector inside: " << std::endl;
+	std::vector<int> inside {40,30,20,10};
+	print_vector_info(inside);
+	return inside;
+}
+
 int main()
 {
 	std::cerr << "User defined types and std::pair ostream overload test" << std::endl
 		<< "$Id$" << std::endl
 		<<"Compiled: " __DATE__ " " __TIME__ << std::endl;
-		
+	
 	std::vector<int> src{4,3,2,1};
 	std::cerr << "vector src: " << std::endl;
 	print_vector_info(src);
@@ -42,11 +50,16 @@ int main()
 	print_vector_info(dst_constructed);
 
 	
-//		std::vector<int> dst_move(std::move(src));
+//	std::vector<int> dst_move(std::move(src));
 	std::vector<int> dst_move(static_cast<std::vector<int> &&>(src));
 	print_vector_info(dst_move);
 		
 	std::cerr <<  "vector src after move: " << std::endl;
 	print_vector_info(src);
+	
+	std::vector<int> returned = return_vector();
+	std::cerr <<  "returned vector: " << std::endl;
+	print_vector_info(returned);
+
 	return 0;
 }
