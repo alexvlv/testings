2606a4aabce6841dd310703e431691035c683a11
AlexVol <alexey.v.v@mail.ru>
2023-06-22 15:00:19 +0300

    IMX8: QT6 GUI - cp1251 test

--- qt/cmake/gui-imx/mainwindow.cpp
+++ qt/cmake/gui-imx/mainwindow.cpp
@@ -1,19 +1,61 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QTextCodec>
+#include <QByteArray>
+#include <QFile>
+
+#include <cassert>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
+    QTextCodec *wincodec = QTextCodec::codecForName("cp1251");
+    assert(wincodec);
+    QTextCodec::setCodecForLocale(wincodec);
+
+    //QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
+
     ui->setupUi(this);
     setWindowFlags(Qt::FramelessWindowHint);
     setAttribute(Qt::WA_NoSystemBackground);
     setAttribute(Qt::WA_TranslucentBackground);
     setAttribute(Qt::WA_TransparentForMouseEvents);
+/*
+    "windows-1251"
+    "cp1251"
+    "ANSI1251"
+    "windows-1251"
+    "cp1251"
+    "ANSI1251"
+*/
+
+    QList<QByteArray> codecs = QTextCodec::availableCodecs();
+    foreach (auto codec, codecs) {
+        if(codec.contains("1251"))
+            qDebug() << codec;
+    }
+
+    QFile f("/mnt/nfs/1251.txt" );
+    if( f.open(QIODevice::ReadOnly|QIODevice::Text) ) {
+        QByteArray wintext = f.readAll();
+        qDebug() << "read" << wintext.size() << "bytes";
+        QString txt = wincodec->toUnicode(wintext);
+        ui->labelWin->setText(txt);
+        QString txtOrig = QString::fromLocal8Bit(wintext);
+        ui->labelWinOrig->setText(txtOrig);
+    }
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    close();
 }
 
