18f04e2f2f4453b15a1024db1739e0ea5a45f6ee
AlexVol <alexvol@local.yurion.ru>
2025-04-29 17:45:03 +0300

    QLabel horizontal size ...

--- qt/qkeywin/MainWindow.cpp
+++ qt/qkeywin/MainWindow.cpp
@@ -1,14 +1,19 @@
 #include "MainWindow.h"
 #include "ui_MainWindow.h"
+#include ".git.h"
 
+//-------------------------------------------------------------------------
 MainWindow::MainWindow(QWidget *parent) :
 	QWidget(parent),
 	ui(new Ui::MainWindow)
 {
 	ui->setupUi(this);
-}
+	ui->lblVersion->setText(APPNAME " GIT rev.: " GIT_REV " at " GIT_DATE " on " GIT_BRANCH " [Build: " BUILD_TIMESTAMP " UTC]");
 
+}
+//-------------------------------------------------------------------------
 MainWindow::~MainWindow()
 {
 	delete ui;
 }
+//-------------------------------------------------------------------------
