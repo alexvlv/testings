c0fd11391f768df62b19cd275406b84cf35585e0
AlexVol <alexvol@local.yurion.ru>
2025-06-04 20:57:05 +0300

    QAudioDevice info(QMediaDevices::defaultAudioOutput());

--- qt/qsound/main.cpp
+++ qt/qsound/main.cpp
@@ -4,6 +4,9 @@
 #include "PlayWav.h"
 #include "MediaPlayer.h"
 
+#include <QAudioDevice>
+#include <QMediaDevices>
+
 #include <QCoreApplication>
 #include <QDebug>
 
@@ -12,6 +15,9 @@ int main(int argc, char *argv[])
 {
     qInfo()<< APPNAME " GIT rev.: " GIT_REV " on " GIT_BRANCH " [Build: " BUILD_TIMESTAMP "]";
     QCoreApplication a(argc, argv);
+	QAudioDevice info(QMediaDevices::defaultAudioOutput());
+	qDebug() << info.supportedSampleFormats();
+	qDebug() << info.preferredFormat();
     //PlayWav play;
     MediaPlayer play;
     return a.exec();
