eeda577bc4ef51c4aef418a0ebb24a4fa813eccc
AlexVol <alexvol@local.yurion.ru>
2026-02-26 21:42:01 +0300

    h264: convert using ffmpeg to TS

--- video/ffmpeg/ff2h264.sh
+++ video/ffmpeg/ff2h264.sh
@@ -15,5 +15,26 @@ to_raw_h264() {
 		-pix_fmt yuv420p \
 		-f h264 $outname
 }
+#to_raw_h264 
 
-to_raw_h264 
+
+#-g 25 -keyint_min 25 -sc_threshold 0 - fixed GOP 25 (not nrcessary for DaVinci decoder)
+
+to_ts() {
+	outname="${input%.*}.ts"
+	echo "$input => $outname"
+
+	ffmpeg -i "$input" \
+		-map 0:v:0 -an -sn \
+		-vf "scale=720:576" \
+		-c:v libx264 -profile:v high \
+		-bf 0 \
+		-g 25 -keyint_min 25 -sc_threshold 0 \
+		-pix_fmt yuv420p \
+		-f mpegts \
+		-streamid 0:0x1011 \
+		$outname
+}
+to_ts
+
+# DvDecad - OK
