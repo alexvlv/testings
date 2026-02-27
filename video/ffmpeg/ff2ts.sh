a724036dc54f35e3ca60104a6755ad018de0ee74
AlexVol <alexvol@local.yurion.ru>
2026-02-27 13:24:51 +0300

    ts stream repack: attempt to fix

--- video/ffmpeg/ff2ts.sh
+++ video/ffmpeg/ff2ts.sh
@@ -7,10 +7,13 @@ repack_to_ts() {
 	outname="${input%.*}_out.ts"
 	echo "$input => $outname"
 	ffmpeg -i "$input" \
+		-err_detect ignore_err \
 		-c:v copy \
+		-bsf:v h264_mp4toannexb \
 		-f mpegts \
 		-streamid 0:0x1011 \
 		$outname
 }
 repack_to_ts
 
+#		-bsf:v "h264_mp4toannexb, h264_metadata=replace" \
