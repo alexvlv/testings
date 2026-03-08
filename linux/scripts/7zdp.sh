73ca535baf2f453c0b5b0d8a8b907b9a0079c1af
AlexVol <alexvol@local.yurion.ru>
2026-03-08 20:35:51 +0300

    7z password

---
+++ linux/scripts/7zdp.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# GIT Rev.: $Format:%cd %cn %h %D$
+``
+[ "$1" != "" ] && outdir="$1"|| outdir=.
+
+psw_file="/var/run/user/$UID/psw"
+[ -s "$psw_file" ] || {
+  echo "Password must be unlocked first!"
+  psw-unlock
+  [ -s "$psw_file" ] || exit 1
+}
+psw=$(<$psw_file)
+
+date=$(/bin/date +%y%m%d-%H%M)
+name=$(basename "${PWD}")
+ext="tar.7z"
+
+fname=${name}-${date}.${ext}
+outfname=${outdir}/${fname}
+
+bytes=$(du -sb ./ | awk '{print $1}')
+log=${outdir}/.${fname}.log
+
+echo "Target: $outfname"
+
+7z a "$outfname" -p${psw} ./
